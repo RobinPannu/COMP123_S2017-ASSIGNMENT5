75ddf49687c71f55eedfe45aad0709bb6faabe4b
Robin Singh <pannusaab1997@gmail>
2017-08-14 15:01:02 -0400

    Add project files.

---
+++ COMP123_S2017 ASSIGNMENT5/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace COMP123_S2017_ASSIGNMENT5
+{
+    partial class BMICalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HeightWeightTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.MetricRadioButton = new System.Windows.Forms.RadioButton();
+            this.ImperialRadioButton = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // HeightWeightTableLayoutPanel
+            // 
+            this.HeightWeightTableLayoutPanel.ColumnCount = 3;
+            this.HeightWeightTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 32.36237F));
+            this.HeightWeightTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 32.36237F));
+            this.HeightWeightTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35.27527F));
+            this.HeightWeightTableLayoutPanel.Location = new System.Drawing.Point(12, 125);
+            this.HeightWeightTableLayoutPanel.Name = "HeightWeightTableLayoutPanel";
+            this.HeightWeightTableLayoutPanel.RowCount = 2;
+            this.HeightWeightTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.HeightWeightTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.HeightWeightTableLayoutPanel.Size = new System.Drawing.Size(280, 129);
+            this.HeightWeightTableLayoutPanel.TabIndex = 0;
+            // 
+            // MetricRadioButton
+            // 
+            this.MetricRadioButton.AutoSize = true;
+            this.MetricRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MetricRadioButton.Location = new System.Drawing.Point(65, 44);
+            this.MetricRadioButton.Name = "MetricRadioButton";
+            this.MetricRadioButton.Size = new System.Drawing.Size(70, 24);
+            this.MetricRadioButton.TabIndex = 1;
+            this.MetricRadioButton.TabStop = true;
+            this.MetricRadioButton.Text = "Metric";
+            this.MetricRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // ImperialRadioButton
+            // 
+            this.ImperialRadioButton.AutoSize = true;
+            this.ImperialRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ImperialRadioButton.Location = new System.Drawing.Point(151, 44);
+            this.ImperialRadioButton.Name = "ImperialRadioButton";
+            this.ImperialRadioButton.Size = new System.Drawing.Size(83, 24);
+            this.ImperialRadioButton.TabIndex = 2;
+            this.ImperialRadioButton.TabStop = true;
+            this.ImperialRadioButton.Text = "Imperial";
+            this.ImperialRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // BMICalculator
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(304, 442);
+            this.Controls.Add(this.ImperialRadioButton);
+            this.Controls.Add(this.MetricRadioButton);
+            this.Controls.Add(this.HeightWeightTableLayoutPanel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "BMICalculator";
+            this.Text = "BMI CALCULATOR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel HeightWeightTableLayoutPanel;
+        private System.Windows.Forms.RadioButton MetricRadioButton;
+        private System.Windows.Forms.RadioButton ImperialRadioButton;
+    }
+}
+
